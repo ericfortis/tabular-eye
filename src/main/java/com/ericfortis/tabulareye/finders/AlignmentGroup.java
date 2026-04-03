5b3515cf1463f1192fda91ebffc1a7edbdf4c34c
Eric Fortis <efortis@gmail.com>
2026-04-03 02:07:19 -0400

    ref

--- src/main/java/com/ericfortis/tabulareye/finders/AlignmentGroup.java
+++ src/main/java/com/ericfortis/tabulareye/finders/AlignmentGroup.java
@@ -3,9 +3,6 @@ package com.ericfortis.tabulareye.finders;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * One group of properties that should be aligned together.
- */
 public class AlignmentGroup {
 	public final List<PropInfo> props = new ArrayList<>();
 }
