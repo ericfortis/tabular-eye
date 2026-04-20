4c06dd673fd3383ddd0dd718f4b72302a2104a1b
Eric Fortis <efortis@gmail.com>
2026-04-19 21:23:31 -0400

    refactor ts

--- src/main/java/com/ericfortis/tabulareye/detectors/TsInterfaceDetector.java
+++ src/main/java/com/ericfortis/tabulareye/detectors/TsInterfaceDetector.java
@@ -1,8 +1,9 @@
 package com.ericfortis.tabulareye.detectors;
 
 import com.intellij.lang.javascript.psi.ecma6.TypeScriptObjectType;
+import com.intellij.lang.javascript.psi.ecma6.TypeScriptPropertySignature;
+import com.intellij.lang.javascript.psi.ecma6.TypeScriptTypeMember;
 import com.intellij.lang.javascript.psi.ecma6.impl.TypeScriptObjectTypeImpl;
-import com.intellij.lang.javascript.psi.jsdoc.JSDocComment;
 import com.intellij.openapi.editor.Document;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -33,31 +34,31 @@ public class TsInterfaceDetector extends AlignmentDetector {
 	}
 
 	@Nullable
-	static PropInfo describeKV(PsiElement prop) {
-		PsiElement keyElement = null;
-		PsiElement colonElement = null;
+	static PropInfo describeKV(TypeScriptTypeMember prop) {
+		if (!(prop instanceof TypeScriptPropertySignature))
+			return null;
 
-		var child = prop.getFirstChild();
+		var keyElem = ((TypeScriptPropertySignature) prop).getIdentifyingElement();
+		if (keyElem == null)
+			return null;
+
+		PsiElement colonElem = null;
+		var child = keyElem.getNextSibling();
 		while (child != null) {
-			var node = child.getNode();
-			if (node != null) {
-				var typeName = node.getElementType().toString();
-				if ("JS:IDENTIFIER".equals(typeName) || "JS:STRING_LITERAL".equals(typeName)) {
-					keyElement = child;
-				} else if ("JS:COLON".equals(typeName)) {
-					colonElement = child;
-				}
+			if (":".equals(child.getText())) {
+				colonElem = child;
+				break;
 			}
 			child = child.getNextSibling();
 		}
 
-		if (keyElement == null || colonElement == null)
+		if (colonElem == null)
 			return null;
 
 		return new PropInfo(
-			 keyElement.getText(),
-			 keyElement.getTextRange().getStartOffset(),
-			 colonElement.getTextRange().getStartOffset()
+			 keyElem.getText(),
+			 keyElem.getTextRange().getStartOffset(),
+			 colonElem.getTextRange().getStartOffset()
 		);
 	}
 }
